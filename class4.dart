7303ab080c0526b82903fed93459b487fafadd59
noumanzindani <noumai41@gmail.com>
2024-07-02 21:44:15 +0500

    added class4.dart

--- class4.dart
+++ class4.dart
@@ -10,7 +10,15 @@ void main() {
   // print(studentnames3.last);
   //   List<String> studentnames4 = ["imran", "nouman", "ali"];
   // print(studentnames4.firstOrNull);
-  List<String> studentnames5 = ["imran", "nouman", "ali","Anus","khan","SAUD","FARAHAN"];
+  // List<String> studentnames5 = [
+  //   "imran",
+  //   "nouman",
+  //   "ali",
+  //   "Anus",
+  //   "khan",
+  //   "SAUD",
+  //   "FARAHAN"
+  // ];
   // print(studentnames5.hashCode);
   // print(studentnames5[2]);
   // print(studentnames5.elementAt(2));
@@ -19,5 +27,23 @@ void main() {
   // int listlenght = studentnames5.length;
   // print(listlenght - 1);
   // print(studentnames5[studentnames5.length -1]);
-   
+  // studentnames5.replaceRange(studentnames5.length-1,studentnames5.length, ["fahad"]);
+  // print(studentnames5);
+  // studentnames5.replaceRange(0, 1, ["akber"]);
+  // print(studentnames5);
+  // List<int> studentnames5 = [1, 234, 468, 512, 0, 5468, 999, 102];
+  // print(!studentnames5.isNotEmpty);
+  // studentnames5.clear();
+
+//  studentnames5.sort();
+
+  // print(studentnames5.isEmpty);
+  // studentnames5.add(66);
+  // studentnames5.addAll([23, 22, 333]);
+  // var studentnames = List.of(studentnames5.reversed);
+  // // studentnames5.remove(5468);
+  // studentnames5.removeAt(0);
+  // studentnames5.removeWhere((e) => e < 50);
+  // studentnames5.retainWhere((e) => e > 50);
+  // print(studentnames5.reversed);
 }
