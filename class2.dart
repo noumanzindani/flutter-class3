fdd03d8f82157a8eb4663778576f74a7a1b6ca45
noumanzindani <noumai41@gmail.com>
2024-07-02 19:14:20 +0500

    added class4.dart

--- class2.dart
+++ class2.dart
@@ -1,19 +1,12 @@
 void main() {
- int num7 = 30;
-  if (num7 % 5 == 0 || num7 % 7 == 0) {
-
-
-
+ int num7 = 3;
+  if (num7 % 5 == 0 || num7 % 7 == 0 ) {
     if (num7 % 5 == 0 && num7 %7==0) {
       print("num is divisabel by both");
     } else if (num7 % 7 == 0) {
       print('number is divisabel by 7');
     } else {
       print("number is divisabel by 5");
-    }
- } 
- 
- else {
-    print("number is not divisabel by 5 or 7");
+    }} else {print("number is not divisabel by 5 or 7");
   }
 }
---
+++ class4.dart
@@ -0,0 +1,23 @@
+void main() {
+  //index length sa aik kam hoga index always start with zero
+  //   List<String> studentNames = ["Nouman", "Arriji", 'irtiza', "ahmed", "Anus"];
+  // print(studentNames[4]);
+  //   List<String> studentnames1 = ["imran", "nouman", "ali"];
+  // print(studentnames1.reversed);
+  //   List<String> studentnames = ["imran", "nouman", "ali"];
+  // print(studentnames.indexed);
+  //   List<String> studentnames3 = ["imran", "nouman", "ali"];
+  // print(studentnames3.last);
+  //   List<String> studentnames4 = ["imran", "nouman", "ali"];
+  // print(studentnames4.firstOrNull);
+  List<String> studentnames5 = ["imran", "nouman", "ali","Anus","khan","SAUD","FARAHAN"];
+  // print(studentnames5.hashCode);
+  // print(studentnames5[2]);
+  // print(studentnames5.elementAt(2));
+  // studentnames5[0] = "urwah";
+  // print(studentnames5[0]);
+  // int listlenght = studentnames5.length;
+  // print(listlenght - 1);
+  // print(studentnames5[studentnames5.length -1]);
+   
+}
