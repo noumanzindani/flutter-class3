b79ad82ea23974ea1bf3dc1fe8f53552c00e575f
noumanzindani <noumai41@gmail.com>
2024-07-02 23:17:39 +0500

    added class5.dart

---
+++ class5.dart
@@ -0,0 +1,8 @@
+void main() {
+  // List studentrecord = ['bilal', 'taber'];
+  Map studentrecord = {"name": "noumnan", "rollno": 19};
+  print(studentrecord.values);
+  print(studentrecord.keys);
+  print(studentrecord["name"]);
+  print(studentrecord);
+}
