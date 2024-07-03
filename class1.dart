d6a9515a822660421aca311cb3e4a3aa26c5c36f
noumanzindani <noumai41@gmail.com>
2024-07-04 01:23:02 +0500

    added class5.dart and class1.dart

--- class1.dart
+++ class1.dart
@@ -1,13 +1,5 @@
 void main() {
-  print("class 1 file");
 
-  int num5 = (50);
-  int num6 = (51);
-  if (num5 >= num6) {
-    print("num5 is greater");
-     
-  } else {
-    print('num6 is greater');
-  }
+
 
 }
--- class5.dart
+++ class5.dart
@@ -4,21 +4,117 @@ void main() {
 //   print(studentrecord.keys);
 //   print(studentrecord["name"]);
 //   print(studentrecord);
-  List<String> studentname = ["noumnba," "imran"];
+  // List<String> studentname = ["noumnba," "imran"];
   // Map studentrecord = {"name": "noumnan", "rollno": 19};
-  Map studentlist = {
-    "name": "nouman",
-    "age": "24",
-    "name1": "Nouman",
-    "class": "13"
-  };
-  // print(studentlist.keys);
+  // Map studentlist = {
+  //   "name": "nouman",
+  //   "age": "24",
+  //   "name1": "Nouman",
+  //   "class": "13"
+  // };
+  // // print(studentlist.keys);
   // print(studentlist.values);\
   // print(studentlist["name1"]);
 //   // print(studentlist["age"]);
 // Map student1records2 = {"name": "imran", "age": "25", "class": "15"};
 //   Map student1records3 = {"name": "Urwah", "age": "26", "class": "16"};
-  Map student1records1 = {"name": ["nouman","imarn","urwah"], "age":["24","26","27"] , "class": ["14","15","16"]};
-  var abc = student1records1["name"];
-  print(abc[0]);
+  // Map student1records1 = {
+  //   "name": ["nouman", "imarn", "urwah"],
+  //   "age": ["24", "26", "27"],
+  //   "class": ["14", "15", "16"]
+  // };
+  // var abc = student1records1["name"];
+  // // print(abc[0]);
+  // print(student1records1["name"][0]);
+
+  // print(student1records1["age"][0]);
+
+  //   List student = [
+//     {"name":"nouman", "rollno":"12"},
+// {"name":"imran","rollno":"13"} ,{"name":"maaz","rollno":"14"} ];
+// print(student[1]["name"]);
+// print(student[1]["rollno"]);
+
+  // Map<String,dynamic> studentsRecord = {
+  //   "name": "Nouman",
+  //   "age": 19,
+  //   "class":12};
+
+  // // var fruit = {1: "appel", 2: "banan", 3: "pineappel"};
+  // // // fruit[5] = "mango";
+  // // var abc = fruit.putIfAbsent(5, () => "pich");
+  // // fruit.addAll({7: "pumkin", 8: "lecih"});
+  // print(abc);
+
+  // var words = {
+  //   1: "sky",
+  //   2: "fly",
+  //   3: "die",
+  //   4: "bye",
+  //   5: "rock",
+  //   6: "ocean",
+  //   7: "falcon",
+  //   8: "ribbon",
+  // };
+
+  // words.remove(2);
+
+  // words.addAll({9: 'king'});
+
+  // words.removeWhere((key, value) => value.startsWith('f'));
+
+  // words.removeWhere((key, value) => key.isOdd);
+
+  // words.removeWhere(
+  //   (key, value) => key.isEven,
+  // );
+
+  // words.isNotEmpty;
+
+  // words.clear();
+
+  // print(words);
+
+//practice quiation
+
+  // List haha = [
+  //   {
+  //     0: 0,
+  //     1: [1],
+  //   },
+  //   {"2","3"},
+  //   {
+  //     1: {
+  //       "hehe": [
+  //         {
+  //           "hehe": {"haha": "bye"}
+  //         }
+  //       ],
+  //     }
+  //   }
+  // ];
+  // // print(haha[2][1]["hehe"][0]["hehe"]["haha"]);
+
+//  Map
+  // var f1 = {1: "appel", 2: "banana"};
+  // var f2 = {3: "Orange"};
+  // var f3 = {4: "mango"};
+
+//  variabel
+  // var fruit = {}
+
+// method 1
+  // ..addAll(f1)
+  // ..addAll(f2);
+  // print(fruit);
+
+// method 2
+// var allfruits={...f1,...f2,...f3};
+
+//print
+// print(allfruits);
+
+  // var fruit = {1: "appel", 2: "banana", 3: "mango"};
+
+
 }
